e28f5d2e28666cfe022c506fa65d3b67314a84b9
Felipe Gallesco <cronowish@gmail.com>
2026-02-09 19:32:26 -0300

    Add auth and cart routes and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { About } from './features/about/about';
+import { Auth } from './features/auth/auth';
+import { Cart } from './features/cart/cart';
+import { NotFound } from './features/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'about', component: About },
+    { path: 'auth', component: Auth },
+    { path: 'cart', component: Cart },
+    { path: '**', component: NotFound }
+];
